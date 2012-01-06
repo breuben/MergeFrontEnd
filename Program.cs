4f151e3aa00c624fadfb34335a859fa35a0160f0
breuben <breuben@gmail.com>
2012-01-05 16:18:24 -0800

    Front end for p4merge for use from within Visual Studio.
    When calling an external merge tool, Visual Studio supplies
    an argument for where the merged file should be saved, but
    it does not create this file. Upon startup, p4merge checks
    for the existence of such a file, and if one does not exist,
    it returns an error.
    
    This program takes the parameter passed from Visual Studio,
    creates an empty file, and in turn calls p4merge.
    
    Note: when configuring this from within Visual Studio,
    the correct order of the arguments is %3 %1 %2 %4.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MergeFrontEnd
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				ProcessStartInfo startInfo = new ProcessStartInfo();
+
+				string argString = "";
+				foreach (string arg in args)
+					argString += string.Format(" \"{0}\"", arg);
+
+				if (argString.Length > 0)
+					argString = argString.Substring(1);
+
+				argString = "-db " + argString;
+
+				string mergedFilePath = args[3];
+				File.WriteAllText(mergedFilePath, "");
+
+				startInfo.FileName = @"C:\Program Files\Perforce\p4merge.exe";
+				startInfo.Arguments = argString;
+
+				Process mergeTool = Process.Start(startInfo);
+				mergeTool.WaitForExit();
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show("Error:\n" + e.ToString());
+			}
+		}
+	}
+}
